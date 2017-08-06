fe7ea217926a1458ec449f732a7ffca8d13da663
Vincent RAMPAL <vincent.rampal@gmail.com>
2017-08-06 13:33:37 +0200

    Fixed ecowatt parsing

--- src/main/java/vrampal/protoinfo/ecowatt/EcowattLevel.java
+++ src/main/java/vrampal/protoinfo/ecowatt/EcowattLevel.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum EcowattLevel {
 
-  REASONABLE(1, "vert"),
-  VERY_HIGH(2, "orange"),
-  EXTREME(3, "rouge");
+  REASONABLE(1, "Verte"),
+  VERY_HIGH(2, "Orange"),
+  EXTREME(3, "Rouge");
 
   @Getter
   private final int level;
@@ -18,7 +18,7 @@ public enum EcowattLevel {
 
   public static EcowattLevel valueOfColor(String color) {
     for (EcowattLevel level : values()) {
-      if (level.getColor().equals(color)) {
+      if (level.getColor().equalsIgnoreCase(color)) {
         return level;
       }
     }
